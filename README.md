f8ff53bc5d847be007b1e9fb40b7d48c0c846165
volvov54 <volvov54@gmail.com>
2022-05-12 13:03:38 +0300

    Created a file "computers.json"

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 #Step 1
 Create Control
+
+#Step 2
+Read file 
---
+++ src/main/kotlin/org/vva/comps/models/Computers.kt
@@ -0,0 +1,7 @@
+package org.vva.comps.models
+
+class Computer(
+    val name: String,
+    val login: String,
+    val ip: String
+)
