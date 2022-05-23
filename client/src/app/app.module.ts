9d7f63a5238d451eccb812b4308def7d383be0a7
volvov54 <volvov54@gmail.com>
2022-05-23 16:57:27 +0300

    Step 6 - Add header nav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,15 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {HeaderComponent} from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, NavComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
